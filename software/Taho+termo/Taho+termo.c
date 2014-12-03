0eaa7267f7bde7ce4d21c16927106a7fe1aaf724
Pavel Kartsev <modemoid@gmail.com>
2014-12-03 22:18:27 +0300

    added empty AtmelStudio 6.0 project

---
+++ software/Taho+termo/Taho+termo.c
@@ -0,0 +1,17 @@
+/*
+ * Taho_termo.c
+ *
+ * Created: 03.12.2014 22:17:22
+ *  Author: kartsev
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
