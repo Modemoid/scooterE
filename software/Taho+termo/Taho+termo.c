8cba8156c2f159f933ef8a3aff7868dd6d04a4cf
Pavel Kartsev <modemoid@gmail.com>
2014-12-04 10:06:46 +0300

    Added software base diagram

--- software/Taho+termo/Taho+termo.c
+++ software/Taho+termo/Taho+termo.c
@@ -6,12 +6,40 @@
  */ 
 
 
+#define F_CPU 1000000UL
 #include <avr/io.h>
+#include <avr/iom8.h>
+#include "interrupt.h"
+#include "delay.h"
+
+#define DEBUG_UART
+#define taho
+#define thermometer_RES
+//#define thermometer_1wire
+//#define thermometer_i2c
+
 
 int main(void)
 {
+	//setup section
+			#ifdef thermometer_RES
+			//put ADC setup here
+			#endif
+	//end of setup section
     while(1)
     {
+		
+		#ifdef taho
+		
+		#endif 
+		
+		#ifdef thermometer_RES
+		
+		#endif
+		
+		#ifdef DEBUG_UART
+		
+		#endif
         //TODO:: Please write your application code 
     }
 }
