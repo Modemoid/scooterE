12cae1d2891bc96ad5ed75e19c460f5f10ce8607
Pavel Kartsev <modemoid@gmail.com>
2014-12-15 15:33:41 +0300

    light_control software added

---
+++ software/Light_control/Light_control/Light_control.c
@@ -0,0 +1,17 @@
+/*
+ * Light_control.c
+ *
+ * Created: 15.12.2014 15:32:42
+ *  Author: kartsev
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
