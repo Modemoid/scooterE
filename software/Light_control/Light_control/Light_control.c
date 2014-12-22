1163cf9cf082374f2010387bb42f9eba253831fa
Pavel Kartsev <modemoid@gmail.com>
2014-12-22 07:47:40 +0600

    Added strobe timer

--- software/Light_control/Light_control/Light_control.c
+++ software/Light_control/Light_control/Light_control.c
@@ -11,17 +11,48 @@
 #define HeadLightControl
 //#define i2c_Comm
 #define Strobe
+//#define Strobe_Bink
+#define PanelLight
+
+#define Strobe_TIME 100 //*0.15s = strobe output HI time after single button press
 
 
 #include <avr/io.h>
 #include <avr/delay.h>
 #include <avr/interrupt.h>
 
+//gobal VAR
+char OutPort; //for output port D (use only 6 end bit, 0 and 1 bit - UART now reserved for future options)
+char Strobe_on = 0; //for strobe
+//End of globa var
+ISR(TIMER0_OVF_vect)
+{
+ #ifndef Strobe_Bink
+	 if (Strobe_on)
+	 {
+ 	 OutPort |= 0b00100000;
+	  Strobe_on++;
+     }else {
+	 OutPort &= 0b11011111;
+	}
+	if (Strobe_on > Strobe_TIME)
+	{
+	Strobe_on = 0; 		
+	}
+#endif
+}
+
 
 
 int main(void)
 {
-char butt,butt1, OutPort;
+	sei();//разрешаем прерывания глобально
+char butt,butt1;
+//настройка 8бит таймера 
+TCCR0|=(1<<CS00)|(1<<CS01); // Тактировать с коэффициентом 64. 1 переполнение = 0.016384 сек
+TIMSK|=(1<<TOIE0);
+
+
 
 	//настройка портов для кнопок
 	DDRC = 0b00110000;  //kb port
@@ -51,10 +82,8 @@ char butt,butt1, OutPort;
 	butt1 = 	PINC&0b00001000;
 	if (!butt1) 
 	{
-	OutPort |= 0b00100000;
-	}else {
-		OutPort &= 0b11011111;
-	}
+		Strobe_on = 1;
+		}
 
 #endif
 #ifdef HeadLightControl
