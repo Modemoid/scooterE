5ecc7d0d8a65795b9d212ffea35dd114f5f7a99f
Pavel Kartsev <modemoid@gmail.com>
2015-03-18 15:26:39 +0600

    added MicroClimat cheap version template

---
+++ MicroClimater/Cheap Controller/CheapController/CheapController.c
@@ -0,0 +1,21 @@
+/*
+ * CheapController.c
+ *
+ * Created: 18.03.2015 15:19:55
+ *  Author: kartsev
+ */ 
+
+#define name CheapController
+#define DEBUG
+
+#include <avr/io.h>
+
+char State=0;
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
